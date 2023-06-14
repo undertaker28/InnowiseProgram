a61ad90b73a4a696db23b651044b5c21747a597d
undertaker28 <pavel.starovoytovvv@gmail.com>
2023-06-14 20:36:41 +0300

    Add implementation of the pattern "Facade"

---
+++ DesignPatterns/DesignPatterns.playground/Pages/Facade.xcplaygroundpage/Contents.swift
@@ -0,0 +1,51 @@
+class UserManager {
+    private var apiManager: ApiManager
+    private var cacheManager: CacheManager
+
+    init(apiManager: ApiManager = ApiManager(),
+         cacheManager: CacheManager = CacheManager()) {
+        self.apiManager = apiManager
+        self.cacheManager = cacheManager
+    }
+    
+    func getUserData() -> String {
+        if let cachedData = cacheManager.getCachedUserData() {
+            print("Retrieved cached user data")
+            return cachedData
+        }
+        
+        let apiData = apiManager.getUserData()
+        cacheManager.cacheUserData(apiData)
+        print("Retrieved user data from API and cached it")
+        
+        return apiData
+    }
+}
+
+class ApiManager {
+    func getUserData() -> String {
+        return "User data from API"
+    }
+}
+
+class CacheManager {
+    func getCachedUserData() -> String? {
+        return "Cached user data"
+    }
+    
+    func cacheUserData(_ data: String) {
+        print("User data cached: \(data)")
+    }
+}
+
+class Client {
+    static func getUserData(userManager: UserManager) {
+        let userData = userManager.getUserData()
+        print("Retrieved user data: \(userData)")
+    }
+}
+
+let apiManager = ApiManager()
+let cacheManager = CacheManager()
+let userManager = UserManager(apiManager: apiManager, cacheManager: cacheManager)
+Client.getUserData(userManager: userManager)
